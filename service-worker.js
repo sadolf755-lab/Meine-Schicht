02ce261d60a742df4241431391a44d1534b7ae22
sadolf755-lab <sadolf755@gmail.com>
2026-08-19 18:14:13 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'meine-schicht-v2';
+const CACHE = 'meine-schicht-v3';
 
 const ASSETS = [
   './index.html',
@@ -6,29 +6,44 @@ const ASSETS = [
   './icon.svg'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    fetch(event.request).then(function(response) {
+      var copy = response.clone();
+
+      caches.open(CACHE).then(function(cache) {
+        cache.put(event.request, copy);
+      });
+
       return response;
-    }).catch(() => caches.match(event.request))
+    }).catch(function() {
+      return caches.match(event.request);
+    })
   );
 });
